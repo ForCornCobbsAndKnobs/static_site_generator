19716cc52e08baed1da37c8cde585619386cda88
ForCornCobbsandKnobs <nicholasscottfuller2077@gmail.com>
2025-10-04 16:37:54 -0400

    added generate page recursive

--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ from static_to_public import static_to_public
 from gen_content import generate_page
 from pathlib import Path
 
-
-def get_index_md():
-    base = Path("./content")
-    return list(base.rglob("index.md"))
-
-def src_to_dst(src: Path) -> Path:
+def src_to_dst(src: Path, dest_root: Path) -> Path:
     rel = src.relative_to("content")
-    dst = Path("public") / rel
+    dst = dest_root / rel
     return dst.with_suffix(".html")
+
+def generate_page_recursive(dir_path_content:str, template_path: str, dest_dir_path: str) -> None:
+    content_root = Path(dir_path_content)
+    dest_root = Path(dest_dir_path)
+
+    for md in content_root.rglob("index.md"):
+        dst = src_to_dst(md, dest_root)
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        generate_page(str(md), template_path, str(dst))
+
 
     
 
@@ -21,11 +26,7 @@ def main():
     public_dir = os.path.join(here, '..', 'public')
     static_to_public(static_dir, public_dir)
 
-    index_mds = get_index_md()
-    for index_md in index_mds:
-        dst = src_to_dst(index_md)
-        dst.parent.mkdir(parents=True, exist_ok=True)
-        generate_page(str(index_md), './template.html', str(dst) )
+    generate_page_recursive("./content", './template.html', './public')
 
 if __name__ == "__main__":
     main()
